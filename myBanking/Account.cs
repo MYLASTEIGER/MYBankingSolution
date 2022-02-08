b75fe466c10449fc436d86e5e7f28d39e21400df
myla steiger <mrs.steiger@gmail.com>
2022-02-08 16:23:59 -0500

    Add project files.

---
+++ myBanking/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myBanking {
+    public class Account {
+        private static int NextAccountNumber { get; set; } = 1;
+        public decimal Balance { get; set; }
+        public int AccountNumber { get; set; }
+        public string Description { get; set; }
+
+        public bool Deposit(Decimal Amount) {
+            if (Amount <= 0) {
+                throw new Exception("Amount must be positive.");
+            }
+            Balance = Balance + Amount;
+            return true;
+        }
+        public bool Withdrawal(Decimal Amount) {
+            if (Amount <= 0) {
+                throw new Exception("Amount must be positive.");
+            }
+            if (Amount > Balance) {
+                throw new Exception("Insufficient Funds.");
+            }
+            Balance = Balance - Amount;
+            return true;
+        }
+
+        public bool Transfer(Decimal Amount, Account ToAccount) {
+            try {
+                Withdrawal(Amount);
+            } catch (Exception) {
+                throw new Exception("Withdrawal Failed in Transfer.");
+            }
+            ToAccount.Deposit(Amount);
+            return true;
+        }
+        public void Debug() {
+            Console.WriteLine($"{AccountNumber}|{Description}|{Balance:c}");
+        }
+        public Account() {
+            AccountNumber = NextAccountNumber;
+            NextAccountNumber++;
+            Description = "New Account";
+            Balance = 0;
+
+        }
+
+
+
+    }
+
+}
+    
---
+++ myBanking/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace myBanking {
+    class Program {
+        static void Main(string[] args) {
+            Account acct1 = new Account();
+            acct1.Deposit(500);
+            acct1.Withdrawal(200);
+            acct1.Debug();
+
+            Account acct2 = new Account();
+            acct2.Description = "my 2nd account";
+            acct2.Deposit(1000);
+            acct2.Withdrawal(350);
+            acct2.Debug();
+
+            acct2.Transfer(100, acct1);
+            acct1.Debug();
+            acct2.Debug();
+
+        }
+    }
+}
